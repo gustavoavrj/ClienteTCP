96600b4fc3c25995af8ade47bea7ada911ee2ae0
gustavoavrj <gustavo.avrj@gmail.com>
2021-03-24 00:19:46 -0600

    fix protocol

--- src/Cliente/Cliente.java
+++ src/Cliente/Cliente.java
@@ -4,12 +4,14 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 import Conexion.Conexion;
+import Protocolo.Protocolo;
 import java.util.InputMismatchException;
 public class Cliente extends Conexion{
     public Cliente() throws IOException{super("cliente");} //Se usa el constructor para cliente de Conexion
 
     public void startClient() //Método para iniciar el cliente
     {
+        Protocolo protocolo = new Protocolo();
         try
         {
             //Flujo de datos hacia el servidor
@@ -22,41 +24,53 @@ public class Cliente extends Conexion{
                 double radio = 0.0;
                 double altura = 0.0;
                 double lado = 0.0;
+                String tipoO = "";
+                String tipoF = "";
 
                 switch (figura) {
                     case 1:
                         radio = validarDouble(scanner,"Ingresa el radio:  ");
                         altura =  validarDouble(scanner,"Ingresa la altura:  ");
+                        tipoF = "CL";
                         break;
                     case 2:
                         radio = validarDouble(scanner,"Ingresa el radio:  ");
+                        tipoF = "ES";
                         break;
                     case 3:
                         radio = validarDouble(scanner,"Ingresa el radio:  ");
                         altura = validarDouble(scanner,"Ingresa la altura:  ");
+                        tipoF = "CO";
                         break;
                     case 4:
                         lado = validarDouble(scanner,"Ingresa la lado:  ");
+                        tipoF = "CU";
                         break;
                     case 5:
                         lado = validarDouble(scanner,"Ingresa la lado:  ");
                         altura = validarDouble(scanner,"Ingresa la altura:  ");
+                        tipoF = "PR";
                         break;
                     case 6:
                         altura = validarDouble(scanner,"Ingresa la altura:  ");
                         lado =validarDouble(scanner,"Ingresa el lsfo:  ");
+                        tipoF = "PI";
                         break;
                 }
-                salidaServidor.writeChar(figura);
-                salidaServidor.writeChar(tipo);
-                salidaServidor.writeDouble(radio); //radio
-                salidaServidor.writeDouble(altura); //altura
-                salidaServidor.writeDouble(lado); //lado
+                switch (tipo){
+                    case 1:
+                        tipoO = "A";
+                        break;
+                    case 2:
+                        tipoO = "V";
+                }
+                String mensaje = protocolo.codificarCliente(tipoO,tipoF,altura,lado,radio);
+                salidaServidor.writeUTF(mensaje);
             }
 
 
             System.out.println("Resultado: ");
-            double resultado = datosRecepcion.readDouble();
+            String resultado = protocolo.decodificarCliente(datosRecepcion.readUTF());
             System.out.print(resultado);
 
 
